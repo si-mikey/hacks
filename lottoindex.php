b763bd038fd235c655c4a166a15cc4806328d551
Luis Lopez <mikeylopez@gmail.com>
2012-11-25 17:29:59 -0500

    lottofix

--- lottoindex.php
+++ lottoindex.php
@@ -1,19 +1,21 @@
-<html>
-<head>
 <?php
-
+error_reporting(0);
 
 
 class lottoParser{
 
 	public $data;
+	public $sm = 0;
+	public $rm = 0;
+	public $f2 = 0;
+	public $l2 = 0;
 
 	public function __construct(){
 
-		$json_file = file_get_contents(__DIR__ . "/" . "onepm.json");
+		//$json_file = file_get_contents(__DIR__ . "/" . "onepm.json");
+		$json_file = file_get_contents("http://geekydroid.com/hacks/onepm.json");
 
 		$json_file = json_decode($json_file, TRUE);
-
 		
 		$data = array();
 		
@@ -44,85 +46,166 @@ class lottoParser{
 	}
 
 
+	public function straight_match($num1, $num2, $num3){
 
+	if ( is_int($num1) && is_int($num2) && is_int($num3) ){
+		
 
-	function straight_match($num1, $num2, $num3){
-
-		$consecutive_matches = 0;
-
-		for ($i =0; $i<279; $i++){
-
+		for ($i =0; $i<=279; $i++){
 			
 			$win_nums = $this->data[$i]["win_nums"];
+			
 			if( $win_nums[0] == $num1 && $win_nums[1] == $num2 && $win_nums[2] == $num3 ){
+			
 
-				echo "	There are previous sorteos/drawings that match this result set consecutively<br />";
-				echo "	date => " . $this->data[$i]["date"] . " <br />"; 
-				echo " 	Sorteo => " . $this->data[$i]["sorteo"] . " <br />";  
+				echo "<p><b>Consecutive Numeric Matches:</b></p>";
+				echo "<p>date => " . $this->data[$i]["date"] . " <br />"; 
+				echo " Sorteo => " . $this->data[$i]["sorteo"] . " <br />";  
 				echo "  Name => " . str_replace("<BR>", " ", $this->data[$i]["name"] . " <br />"); 
-				echo "  Winning #'s " . $win_nums[0] . " " . $win_nums[1] . " " . $win_nums[2] . "<br />";
-
-				$consecutive_matches += 1;	      
-
+				echo " Winning #'s: <b>" . $win_nums[0] . " " . $win_nums[1] . " " . $win_nums[2] . "</b></p>";
+				
+				$this->sm += 1;    
+				
 
 		}
+		
+	}
+
+	if ( $this->sm == 0 ) {  echo "<p><b>No consecutive matches located:</b></p><p>  Pattern: <b>$num1, $num2, $num3</b></p>"; }
+
+}
+
+}
+
+
+	public function rev_match($num1, $num2, $num3){
+
+	if ( is_int($num1) && is_int($num2) && is_int($num3) ){
+		
+		$rev_matches = 0;
+
+		for ($i =0; $i<=279; $i++){
+			
+			$win_nums = $this->data[$i]["win_nums"];
+			
+			if( $win_nums[0] == $num3 && $win_nums[1] == $num2 && $win_nums[2] == $num1 ){
+			
+
+				echo "<p><b>Reverse Numeric Matches:</b></p>";
+				echo "<p> date => " . $this->data[$i]["date"] . " <br />"; 
+				echo " Sorteo => " . $this->data[$i]["sorteo"] . " <br />";  
+				echo " Name => " . str_replace("<BR>", " ", $this->data[$i]["name"] . " <br />"); 
+				echo " Winning #'s: " . $win_nums[0] . " " . $win_nums[1] . " " . $win_nums[2] . "</p>";
 				
-		if ( $consecutive_matches == 0 ) {  echo "1. No consecutive matches were found \n"; }else{ echo "weird $consecutive_matches"; }
+				$rev_matches += 1;    
+
+		}
+		
+	}
+
+	if ( $rev_matches == 0 ) {  echo "<p><b>No reverse matches located:</b></p><p>  Pattern: <b>$num3, $num2, $num1</b></p>"; }
+
+}
+
+} 
+ 
+	public function f2_match($num1, $num2, $num3){
+
+	if ( is_int($num1) && is_int($num2) && is_int($num3) ){
+		
+
+		for ($i =0; $i<=279; $i++){
+			
+			$win_nums = $this->data[$i]["win_nums"];
+			
+			if( $win_nums[0] == $num1 && $win_nums[1] == $num2 ){
+			
+
+				echo "<p><b>First two Numeric Matches:</b></p>";
+				echo "<p> date => " . $this->data[$i]["date"] . " <br />"; 
+				echo " Sorteo => " . $this->data[$i]["sorteo"] . " <br />";  
+				echo " Name => " . str_replace("<BR>", " ", $this->data[$i]["name"] . " <br />"); 
+				echo " Winning #'s: <b>" . $win_nums[0] . " " . $win_nums[1] . " </b>" . $win_nums[2] . "</p>";
+				
+				$this->f2 += 1;  
+
+		}
+		
+	}
+
+	if ( $this->f2 === 0 ) {  echo "<p><b>No first two matches:</b></p><p>  Pattern: <b>$num1, $num2,</b> $num3</p>"; }
+
+}
+
+} 
 
 
+
+	public function get_data(){
+	
+		return $this->data;
+	
 	}
 
 
 
+}//END CLASS
+
+
+if ( array_key_exists('numbers', $_GET) ) {
+
+	$numbers =  $_GET["numbers"];
+
+	$numbers = preg_split("/[\s]+/", $numbers);
+	
+	$num1 = trim($numbers[0]);
+	$num2 = trim($numbers[1]);
+	$num3 = trim($numbers[2]);
+	$num1 = (int)$num1;
+	$num2 = (int)$num2;
+	$num3 = (int)$num3;
+	
+	
+	$br = "<br />";
+	
+	if ( $num1 != 0 && $num2 != 0 && $num3 != 0){
+		
+		$game = new lottoParser;	
+		$nums_avail = TRUE;
+	}
 }
-
-
-}
-
-$numbers =  $_POST["numbers"];
-
-if( !$numbers ) { echo "Please enter search criteria."; exit;}
-
-
-
-
-$s = new lottoParser;
-
-
-$numbers = preg_split("/[\s]+/", $numbers);
-
-
-$num1 = trim($numbers[0]);
-$num2 = trim($numbers[1]);
-$num3 = trim($numbers[2]);
-
-
-$br = "<br />";
-
-echo $br;
-
-$s->straight_match($num1, $num2 , $num3);
-
-
-
-
-
-
-
-
-
-
-?> 
+ 	
+/* echo "<pre>";
+	var_dump( $game->get_data() );
+echo "</pre>"; */
+  
+  
+?>
+<html>
+<head>
+ 
 <style>
 *{padding: 0px; margin:0px;}
 body{font-family: Arial, sans-serif, helvetica; font-size: 160%; }
-
 #html,body{height: 100%; width:100%; } 
-
-#content{ padding-top: 10%; padding-left:30%; min-width:500px; }
 #numbers{ height: 35px; width: 200px; font-size: 18px; }
 #submit { height: 35px; width:100px; }
 
+#formlotto{
+	text-align: center;
+	padding-top: 30px;
+}
+
+#str8_match, #rev_match, #f2_match{
+	float: left;
+	width: 420px;
+	margin: 0px 0px 0px 5px;
+	
+}
+#str8_match p , #rev_match p, #f2_match p{
+	border: 3px solid #ddd;
+	margin:0px 0px 3px 0px;
+}
 
 </style>
 
@@ -135,18 +218,47 @@ body{font-family: Arial, sans-serif, helvetica; font-size: 160%; }
 
 	<div id="content">
 	
-		<div id="form">
-			<form action="parselotto.php" method="POST">
+		<div id="formlotto">
+			<form action="lottoindex.php" method="GET">
 			
 				<input type="text" name="numbers" id="numbers" placeholder="Enter desired lottery #'s" />
 				<input type="submit" name="submit" id="submit" />
 
-			</form>			
-
-		</div>
-	
-
+			</form>
 	</div>
+
+		<div id="results">	
+		
+			<div id="str8_match">
+			<?php 
+			
+				if ( $nums_avail === TRUE )	$game->straight_match($num1, $num2 , $num3); 
+				
+			?>
+			</div>
+			
+			<div id="rev_match">
+			<?php 
+			
+				if ( $nums_avail === TRUE )	$game->rev_match($num1, $num2 , $num3); 
+				
+			?>
+			</div>
+			
+			<div id="f2_match">
+			<?php 
+			
+				if ( $nums_avail === TRUE )	$game->f2_match($num1, $num2 , $num3); 
+				
+			?>
+			</div>
+			
+			
+		
+		</div><!--end #swinners -->
+
+
+	</div><!--end #content -->
 
 
 </div>
