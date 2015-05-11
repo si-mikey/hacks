193625d4da3d784539ffcbddcd9cf32a6c0dbb00
= <mikeylopez@gmail.com>
2015-05-10 22:40:39 -0400

    making range inclusive

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby 
 
-(1...100).each do |n|
+(1..100).each do |n|
   puts "#{n} fizz" if n % 3 == 0
   puts "#{n} buzz" if n % 5 == 0
   puts "#{n} fizzbuzz" if n % 3 == 0 && n % 5 == 0
